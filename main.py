532d54680b6adfe434e06fa71254792bba991c02
Marko Black <marko.blck@gmail.com>
2013-05-30 08:19:45 +0300

    added sample of data retrieving from delicious

--- main.py
+++ main.py
@@ -7,7 +7,19 @@ def run_analisys():
     return sdproc.top_matches(recommendations.critics, 'Lisa Rose')
 
 def run_ext_analisys():
-    return pydelicious.get_popular(tag='programming')
+    return init_user_dict('programming')
+
+
+def init_user_dict(tag, count=5):
+    user_dict = {}
+
+    for p1 in pydelicious.get_popular(tag)[0:count]:
+        for p2 in pydelicious.get_urlposts(p1['url']):
+            user = p2['user']
+            user_dict[user] = {}
+
+    return user_dict
+
 
 
 if __name__ == '__main__':
